35e84ddc5d559d9bb599cd8792d2e13f2b2c60e0
Dexmachi <232001371@aluno.unb.br>
2025-02-02 16:55:58 -0300

    setisPago
    sistema EXTREMAMENTE simplificado para garantir que quem controla se é pago ou não é o paciente

--- src/Clientes/Paciente.java
+++ src/Clientes/Paciente.java
@@ -1,12 +1,39 @@
 package Clientes;
 
+import java.util.Scanner;
+
 public class Paciente {
     // atributos //
-    private Pagamento pagamento;
+
     /*
      * linkar este check de pagamento com o get e set Pago de "Clientes.Pagamento"
      * fazer questão de que seja uma função do objeto "Clientes.Paciente" a troca deste check
     */
     private boolean isPago;
+    private int id;
     // funções //
+    private Pagamento pagamento = new Pagamento(new Staff.Prescricoes.Preco(), 1);
+    public Paciente(Pagamento pagamento, int id) {
+        this.pagamento = pagamento;
+        this.id = id;
+    }
+
+    public void setisPago()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite S se você pagou e N se não: ");
+        String sn = sc.nextLine();
+        if(sn.equals("N") || sn.equals("n"))
+        {
+            pagamento.setPago(false);
+        }
+        else if(sn.equals("S") || sn.equals("s"))
+        {
+            pagamento.setPago(true);
+        }
+        else
+        {
+            setisPago();
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
+import Clientes.Pagamento;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         Clientes.Pagamento pagamento = new Clientes.Pagamento(new Staff.Prescricoes.Preco(),  1);
+        Clientes.Paciente paciente = new Clientes.Paciente(pagamento, 1);
 
-        pagamento.setPago(false);
-
+        paciente.setisPago();
         if(pagamento.getPago())
         {
             System.out.println("pago com sucesso!!!");
